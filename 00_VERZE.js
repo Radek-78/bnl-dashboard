3bd76e7ed875bb962442e33082f376d9377d614d
Radek Pekárek <rpekis@gmail.com>
2026-07-20 12:53:07 +0200

    Vyhledání Název/Obsah nezávislé na DOM - doběhne i po přepnutí záložky (v3.1.78)

--- 00_VERZE.js
+++ 00_VERZE.js
@@ -1,8 +1,8 @@
 /**
  * ============================================
  *  POSLEDNI NASAZENI
- *  Verze:    v3.1.77
- *  Nahrano:  20.07.2026 12:38
+ *  Verze:    v3.1.78
+ *  Nahrano:  20.07.2026 12:52
  * ============================================
  * Generovano automaticky skriptem tools/release.ps1 - needituj rucne.
  * Datum odpovida okamziku nahrani kodu (clasp push); nasazeni nove verze
