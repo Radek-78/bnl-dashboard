aa41f30e0125f0584fa0e484a236d60e2ab47501
Radek Pekárek <rpekis@gmail.com>
2026-07-20 15:09:34 +0200

    Sloupec Teo. stav zobrazuje kombinovanou hodnotu s Dodáno (v3.1.88)

--- 00_VERZE.js
+++ 00_VERZE.js
@@ -1,8 +1,8 @@
 /**
  * ============================================
  *  POSLEDNI NASAZENI
- *  Verze:    v3.1.87
- *  Nahrano:  20.07.2026 15:03
+ *  Verze:    v3.1.88
+ *  Nahrano:  20.07.2026 15:09
  * ============================================
  * Generovano automaticky skriptem tools/release.ps1 - needituj rucne.
  * Datum odpovida okamziku nahrani kodu (clasp push); nasazeni nove verze
